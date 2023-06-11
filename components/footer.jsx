d26dcb6767bd2db342d0de8f747aa86fca4f9f95
hanzzakino <emperornezl50@gmail.com>
2023-06-12 00:52:56 +0800

    layout update

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,19 @@
 import styles from '../styles/Footer.module.css'
+import { BsGithub } from 'react-icons/bs'
 
 export default function Footer() {
     return (
         <footer className={styles.footerContainer}>
-            <p>{'Designed & Developed by Hanz Aquino'}</p>
+            <p className={styles.line1}>
+                {'Designed & Developed by Hanz Aquino'}
+            </p>
+            <a
+                className={styles.line2}
+                href="https://github.com/hanzzakino/hanzz"
+            >
+                <BsGithub className={styles.icon} />
+                {'github.com/hanzzakino/hanzz'}
+            </a>
         </footer>
     )
 }
